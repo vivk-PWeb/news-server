e18302519810e728f8db6d6954fac2bf9dff3285
Volcov Oleg <bojikch15@gmail.com>
2022-04-05 18:23:14 +0300

    Add new `news` router in server boot script.
     + Small server script refactor.
     + Update `package` fily by new dependencies.

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const newsRouter = require('./routes/newsRouter');
 
-var app = express();
+const mongoose = require('mongoose');
+
+const mongodbUrl = 'mongodb://localhost:27017/serverNews';
+const mongodbConnect = mongoose.connect(mongodbUrl);
+
+mongodbConnect
+	.then(db => {
+		console.log(`Connected correctly to DB by URL=${mongodbUrl}`);
+	}, err => {
+		console.log(err);
+	});
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
