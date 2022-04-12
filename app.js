43ab155f04179f6701b5976c582611ab3f174dc9
Volcov Oleg <bojikch15@gmail.com>
2022-04-12 15:45:32 +0300

    Setup server boot file to use new view engine `ejs` and new `news` route.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const newsRouter = require('./routes/newsRouter');
+const newsRouter = require('./routes/news');
 
 const mongoose = require('mongoose');
 
@@ -23,8 +23,7 @@ mongodbConnect
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
