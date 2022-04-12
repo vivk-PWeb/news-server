b96a605c550ada208e0196f92a33645cfbe3020b
Volcov Oleg <bojikch15@gmail.com>
2022-04-12 15:43:39 +0300

    Implement `index` route to represent all available articles.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const News = require('../models/news');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
-});
+const router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+// 	res.render('index', { title: 'Express' });
+// });
+
+router
+	.route('/')
+	.get(async (req, res, next) => {
+		const news = await News.find();
+		res.render('index', { news: news, title: 'Home Page'});
+	});
 
 module.exports = router;
