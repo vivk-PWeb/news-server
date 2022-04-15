2fed0e63bef1c1a600d0e97ccbf4c65a0ececa80
Volcov Oleg <bojikch15@gmail.com>
2022-04-15 10:22:14 +0300

    Modify `index` page for new `roles` functional.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const News = require('../models/news');
 
+const Permissions = require('../enums/permissions');
+const Roles = require('../enums/roles');
+
 const router = express.Router();
 
 // /* GET home page. */
@@ -11,11 +14,21 @@ const router = express.Router();
 router
 	.route('/')
 	.get(async (req, res, next) => {
-		const news = await News.find();
+		const news = await News.find().sort({createdAt: -1});
 		const user = res.locals.user;
+
+		if (user === null) {
+			for (let index in news) {
+				if (news[index].permission === Permissions.Private) {
+					delete news[index];
+				}
+			}
+		}
+
 		res.render('index', {
 			news: news,
 			user: user,
+			roles: Roles,
 			title: 'Home Page'
 		});
 	});
