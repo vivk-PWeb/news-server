24cfccd4635f89c3da67e284e225d1af30c94fcc
Volcov Oleg <bojikch15@gmail.com>
2022-04-15 10:18:07 +0300

    Add possibility to check if user is `Editor`.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,39 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
+const Roles = require('../enums/roles');
+
 const requireAuth = (req, res, next) => {
+	if (res.locals.user !== null) {
+		next();
+		return;
+	}
+
 	const token = req.cookies['jwt'];
 
 	if (token) {
 		jwt.verify(token, 'secret', (err, decodedToken) => {
 			if (err) {
 				console.log(err.message);
-				res.redirect('/sign-in');
+				res.redirect('/auth/sign-in');
 			} else {
 				console.log(decodedToken);
 				next();
 			}
 		});
 	} else {
-		res.redirect('/sign-in');
+		res.redirect('/auth/sign-in');
+	}
+};
+
+const requireEditorRole = (req, res, next) => {
+	const user = res.locals.user;
+	const editorValidate = user.role === Roles.Editor;
+
+	if (!editorValidate) {
+		res.redirect('/');
+	} else {
+		next();
 	}
 };
 
@@ -37,4 +55,4 @@ const checkUser = (req, res, next) => {
 	}
 };
 
-module.exports = { requireAuth, checkUser };
+module.exports = { requireAuth, requireEditorRole, checkUser };
